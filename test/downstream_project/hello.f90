d0b87d653bbae4ed7cb8177d6e32df7cb3b54335
Jared Frazier <frazier@iap-kborn.de>
2025-03-13 11:57:37 +0100

    link worksgit-diff  what about include? is this needed? try fftpack without include...

--- test/downstream_project/hello.f90
+++ test/downstream_project/hello.f90
@@ -1,5 +1,9 @@
 ! Use external library HelloSPBLAS 
 PROGRAM main
-    USE mo_square
+    USE mo_square, ONLY: square 
+    REAL :: x = 2 
+    REAL :: y
     PRINT *, "Hello, world!"   
+    y = square(x)
+    PRINT *, "square(x) =", y
 END PROGRAM main 
